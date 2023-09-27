1b4967a9f74063499fd103ceb62f797dd4160928
Décio <deciocavalcantedeandrade@gmail.com>
2023-09-27 17:04:38 -0300

    Adicionando em Post associação has_many comments

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe Post, type: :model do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:text) }
   it { should belong_to(:user) }
+  it { should have_many(:comments) }
 end
