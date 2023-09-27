e5a5806bb583e80c759f98380d12e3206196a4ad
Décio <deciocavalcantedeandrade@gmail.com>
2023-09-27 17:17:56 -0300

    Adicionando validações e association ao User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :posts
+
+  validates :name, presence: true
 end
